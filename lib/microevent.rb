a15d63a93a6924a691574ba4c3f641d304260fb8
Dustin Tinney <tinney@gmail.com>
2015-03-13 13:07:07 -0700

    Sets the hash when the module is included
    This appears to be a more accepted pattern for ivars in a module.

--- lib/microevent.rb
+++ lib/microevent.rb
@@ -1,20 +1,16 @@
 module MicroEvent
   VERSION = "1.0.1".freeze
-
+  @_ = Hash.new{ |h,k| h[k] = [] }
+  
   def bind(event, &fn)
-    fn ? _[event] << fn : raise(ArgumentError, "no block given")
+    fn ? @_[event] << fn : raise(ArgumentError, "no block given")
   end
 
   def unbind(event, &fn)
-    fn ? _[event].delete(fn) : events.delete(event) || []
+    fn ? @_[event].delete(fn) : events.delete(event) || []
   end
 
   def trigger(event, *args)
-    !_[event].each{ |fn| instance_exec(*args, &fn) }.empty?
-  end
-  
-  private 
-  def _
-    @_ ||= Hash.new{ |h,k| h[k] = [] }
+    !@_[event].each{ |fn| instance_exec(*args, &fn) }.empty?
   end
 end
