f450c771ee7eb4af182e717209c5241abf63d732
Jan Lelis <mail@janlelis.de>
2015-03-14 23:36:18 +0100

    update to 1.1.0

--- lib/microevent.rb
+++ lib/microevent.rb
@@ -1,6 +1,6 @@
-eval"module MicroEvent;VERSION='1.0.2'.freeze
-def trigger(e,*#{y="f)@_||=Hash.new{|h,k|h[k]=[]}
-"}!@_[e].dup.map{|g|instance_exec *f,&g}.empty?end
-def #{x="bind(e,&#{y}f ?@_[e]"}<<f :raise(ArgumentError,'no block given')end
-def un#{x}.delete(f):@_.delete(e)||[]end
-end"
+module MicroEvent;VERSION="1.1.0".freeze
+def bind(e,&f)@_||=Hash.new{|h,k|h[k]=[]}
+f ?@_[e]<<f :raise(ArgumentError,"no block given")end
+def unbind(e,&f)f ?@_&&@_[e].delete(f):@_&&@_.delete(e)||[]end
+def trigger(e,*f)!!@_&&!@_[e].dup.map{|g|instance_exec *f,&g}.empty?end
+end
